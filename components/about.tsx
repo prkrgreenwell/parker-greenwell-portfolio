8fb64d8ed65d5c87e39fbef545e413a4d24c11a0
prkrgreenwell <prkrgreenwell@gmail.com>
2024-06-21 23:33:38 -0600

    added header functionality

--- components/about.tsx
+++ components/about.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function About() {
+  const { ref, inView } = useInView();
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("About");
+    }
+  }, [inView, setActiveSection]);
+
   return (
     <motion.section
+      ref={ref}
       className="mb-28 max-w-[45rem] leading-8 text-center sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
